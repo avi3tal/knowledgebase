9c1dc2097039d869c88ea63f48639a56ca80e95d
Avi Tal <avi@sparkbeyond.com>
2020-08-30 23:25:13 +0300

    adding more

--- leetcode/midium/add_two_numbers.py
+++ leetcode/midium/add_two_numbers.py
@@ -13,26 +13,31 @@ class ListNode:
 def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
     dummy = res = ListNode(None)
     addition = 0
-    while l1 and l2:
-        s = l1.val + l2.val + addition
-        if s >= 10:
-            res.next = ListNode(s - 10)
-            addition = 1
-        else:
-            addition = 0
-            res.next = ListNode(s)
-        res = res.next
-        l1 = l1.next
-        l2 = l2.next
+    while l1 or l2:
+        x = y = 0
+        if l1:
+            x = l1.val
+            l1 = l1.next
+        if l2:
+            y = l2.val
+            l2 = l2.next
 
-    if l1:
-        res.next = l1
-    elif l2:
-        res.next = l2
+        s = x + y + addition
+        addition = s // 10
+        res.next = ListNode(s % 10)
+        res = res.next
+
+    if addition:
+        res.next = ListNode(addition)
     return dummy.next
 
 
 a = add_two_numbers(ListNode(2, ListNode(4, ListNode(3))), ListNode(5, ListNode(6, ListNode(4, ListNode(5)))))
+while a:
+    print(a.val)
+    a = a.next
+
+# a = add_two_numbers(ListNode(1), ListNode(9, ListNode(9)))
 while a:
     print(a.val)
     a = a.next
